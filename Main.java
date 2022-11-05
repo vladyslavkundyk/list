2aed4d9046a0b1c8626e3812b2474504487ab9ec
vladyslavkundyk <113207021+vladyslavkundyk@users.noreply.github.com>
2022-11-05 03:40:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,17 +7,17 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		List<Integer> numList = new ArrayList<>();
+		List<Integer> numberList = new ArrayList<>();
 
 		for (int i = 0; i < 10; i++) {
-			numList.add(0 + (int) (Math.random() * 10));
+			numberList.add(0 + (int) (Math.random() * 10));
 		}
 
-		System.out.println(numList);
-		numList.remove(0);
-		numList.remove(0);
-		numList.remove(numList.size() - 1);
-		System.out.println(numList);
+		System.out.println(numberList);
+		numberList.remove(0);
+		numberList.remove(0);
+		numberList.remove(numberList.size() - 1);
+		System.out.println(numberList);
 
 	}
 }

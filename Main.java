d2c60ac8806337541f98beacc9d328212025134e
vladyslavkundyk <113207021+vladyslavkundyk@users.noreply.github.com>
2022-11-06 18:09:44 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,18 +6,21 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+		createList();
+	}
 
-		List<Integer> numberList = new ArrayList<>();
+	public static void createList() {
+		List<Integer> numList = new ArrayList<>();
 
 		for (int i = 0; i < 10; i++) {
-			numberList.add(0 + (int) (Math.random() * 10));
+			numList.add(0 + (int) (Math.random() * 10));
 		}
 
-		System.out.println(numberList);
-		numberList.remove(0);
-		numberList.remove(0);
-		numberList.remove(numberList.size() - 1);
-		System.out.println(numberList);
+		System.out.println(numList);
+		numList.remove(0);
+		numList.remove(0);
+		numList.remove(numList.size() - 1);
+		System.out.println(numList);
 
 	}
 }
